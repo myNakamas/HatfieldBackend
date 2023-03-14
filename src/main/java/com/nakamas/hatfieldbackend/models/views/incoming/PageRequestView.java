55e86c52d54878b10c0e6fc7938608d988f63e95
Angel Prodanov <angel.g.prodanov@gmail.com>
2023-03-15 01:35:52 +0200

    Modify PageRequestView page number

--- src/main/java/com/nakamas/hatfieldbackend/models/views/incoming/PageRequestView.java
+++ src/main/java/com/nakamas/hatfieldbackend/models/views/incoming/PageRequestView.java
@@ -5,6 +5,6 @@ import org.springframework.data.domain.PageRequest;
 public record PageRequestView(Integer pageSize, Integer page) {
     public PageRequest getPageRequest() {
         if(page == null || pageSize==null) return PageRequest.of(0,10);
-        return PageRequest.of(page -1, pageSize);
+        return PageRequest.of(page, pageSize);
     }
 }
