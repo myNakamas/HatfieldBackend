e713fa1afb3b554225074c9dc09e95aa48b36101
Angel Prodanov <angel.g.prodanov@gmail.com>
2023-09-25 21:53:11 +0300

    Paginate Chat messages

--- src/main/java/com/nakamas/hatfieldbackend/repositories/MessageRepository.java
+++ src/main/java/com/nakamas/hatfieldbackend/repositories/MessageRepository.java
@@ -1,6 +1,8 @@
 package com.nakamas.hatfieldbackend.repositories;
 
 import com.nakamas.hatfieldbackend.models.entities.ticket.ChatMessage;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -13,18 +15,18 @@ import java.util.UUID;
 @Repository
 public interface MessageRepository extends JpaRepository<ChatMessage, Long> {
 
-    @Query("from ChatMessage c where (c.receiver.id = ?1 and c.sender.id = ?2) or (c.receiver.id = ?2 and c.sender.id = ?1)")
+    @Query("from ChatMessage c where (c.receiver.id = ?1 and c.sender.id = ?2) or (c.receiver.id = ?2 and c.sender.id = ?1) order by c.timestamp desc")
     List<ChatMessage> findAllByUsers(UUID to, UUID from);
-    @Query("from ChatMessage c where c.ticketId = ?1")
-    List<ChatMessage> findAllByTicket(Long ticketId);
+    @Query("from ChatMessage c where c.ticketId = ?1 order by c.timestamp desc")
+    Page<ChatMessage> findAllByTicket(Long ticketId, PageRequest pageRequest);
 
-    @Query("from ChatMessage c where (c.receiver.id = ?1 or c.sender.id = ?1) and c.ticketId=?2")
-    List<ChatMessage> findAllForClient(UUID clientId, Long ticketId);
+    @Query("from ChatMessage c where (c.receiver.id = ?1 or c.sender.id = ?1) and c.ticketId=?2 order by c.timestamp desc")
+    Page<ChatMessage> findAllForClient(UUID clientId, Long ticketId, PageRequest pageRequest);
 
-    @Query("from ChatMessage c where (c.receiver.id = ?1 and c.sender.id = ?2)")
+    @Query("from ChatMessage c where (c.receiver.id = ?1 and c.sender.id = ?2) order by c.timestamp desc")
     List<ChatMessage> findAllSentToUser(UUID to, UUID from);
 
-    @Query("from ChatMessage c where (c.receiver.id = ?2 and c.sender.id = ?1)")
+    @Query("from ChatMessage c where (c.receiver.id = ?2 and c.sender.id = ?1) order by c.timestamp desc")
     List<ChatMessage> findAllReceivedFromUser(UUID from, UUID to);
 
     @Modifying
