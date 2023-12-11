66d320a5208b2ee1149482ffe982d550f6b680d6
Angel Prodanov <angel.g.prodanov@gmail.com>
2023-12-11 17:41:58 +0200

    change pythonhome location

--- scripts/print.sh
+++ scripts/print.sh
@@ -6,7 +6,6 @@ IMAGE_LOCATION=$3
 
 export BROTHER_QL_PRINTER=$PRINTER_IP
 export BROTHER_QL_MODEL=$PRINTER_MODEL
-export PYTHONHOME=/home/user/.local/lib/python3.10/site-packages
-export PATH=$PATH:/home/user/.local/bin
+export PYTHONHOME=/usr/local/lib/python3.10/site-packages
 
 /home/user/.local/bin/brother_ql -b network -p "$PRINTER_IP" -m "$PRINTER_MODEL" print -l 62 "$IMAGE_LOCATION"
