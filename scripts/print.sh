6c71b3a54774b70ed0e1d0331a9aeff138dde52b
Angel Prodanov <angel.g.prodanov@gmail.com>
2023-12-11 11:45:01 +0200

    update print script

--- scripts/print.sh
+++ scripts/print.sh
@@ -4,4 +4,7 @@ PRINTER_IP=$1
 PRINTER_MODEL=$2
 IMAGE_LOCATION=$3
 
-/home/user/.local/bin/brother_ql -b network -p ${PRINTER_IP} -m ${PRINTER_MODEL} print -l 62 ${IMAGE_LOCATION}
+export BROTHER_QL_PRINTER=$PRINTER_IP
+export BROTHER_QL_MODEL=$PRINTER_MODEL
+
+/home/user/.local/bin/brother_ql -b network -p "$PRINTER_IP" -m "$PRINTER_MODEL" print -l 62 "$IMAGE_LOCATION"
